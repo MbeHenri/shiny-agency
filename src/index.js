dfc3b7b77849f4107ea7e16c5bc75ff1c52d3d96
MbeHenri <henri.mbe@facsciences-uy1.cm>
2024-01-10 06:07:23 +0100

    update1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home/index";
+import Survey from "./pages/Survey";
+import Header from './components/Header';
+import ClientForm from './components/ClientForm';
+import FreelanceForm from './components/FreelanceForm';
+
+// import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey" element={<Survey />}>
+          { /* Nous imbriquons nos composants dans survey */}
+          <Route path="client" element={<ClientForm />} />
+          <Route path="freelance" element={<FreelanceForm />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
